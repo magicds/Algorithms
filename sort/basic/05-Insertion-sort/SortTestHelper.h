ed49011b5edb02c4e86d7b81a9dceab803b70235
cdswyda <cdswyda@hotmail.com>
2018-06-17 16:45:07 +0800

    05

---
+++ sort/basic/05-Insertion-sort/SortTestHelper.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+namespace SortTestHelper {
+
+	// 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
+	int *generateRandomArray(int n, int range_l, int range_r) {
+
+		int *arr = new int[n];
+
+		srand(time(NULL));
+		for (int i = 0; i < n; i++)
+			arr[i] = rand() % (range_r - range_l + 1) + range_l;
+		return arr;
+	}
+
+	// 定义数组打印函数
+	template <typename T>
+	void printArray(T arr[], int n) {
+		for (int i = 0; i < n; i++)
+			cout << arr[i] << " ";
+		cout << endl;
+		return;
+	}
+
+	template<typename T>
+	void testSort(const string &sortName, void(*sort)(T[], int), T arr[], int n) {
+
+		clock_t startTime = clock();
+		sort(arr, n);
+		clock_t endTime = clock();
+
+		assert(isSorted(arr, n));
+		cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+
+		return;
+	}
+
+	// 判读是否排序成功
+	// 判断arr数组是否有序
+	template<typename T>
+	bool isSorted(T arr[], int n) {
+
+		for (int i = 0; i < n - 1; i++)
+			if (arr[i] > arr[i + 1])
+				return false;
+
+		return true;
+	}
+
+	// 整形数组拷贝
+	int* copyIntArray(int a[], int n) {
+		int * arr = new int[n];
+		// copy(a, a + n, arr);
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = a[i];
+		}
+
+		return arr;
+	}
+}
---
+++ sort/basic/05-Insertion-sort/insertionSort.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+void insertionSort(T arr[], int n) {
+	for (int i = 1; i <n; i++)	{
+
+		/*for (int j = i; j > 0; j--) {
+			if (arr[j] < arr[j-1]) {
+				swap(arr[j], arr[i]);
+			}
+			else {
+				break;
+			}
+		}*/
+		for (int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+			swap(arr[j], arr[j-1]);
+		}
+
+	}
+}
